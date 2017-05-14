518b7a793ed893f590735f403fb2655bf5307f7b
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-05-14 17:21:57 -0400

    create first page router

--- main.py
+++ main.py
@@ -11,3 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import webapp2
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+
+class Handler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        params['user'] = self.user
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class MainPage(BlogHandler):
+
+    def get(self):
+        self.write('Hello, Udacity!')
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
