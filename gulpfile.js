34fd5d84c765e2c0bb59f702e712ccbd17581f14
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-05-14 17:55:50 -0400

    add gulp task running

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-csso');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+gulp.task('default', [], function() {
+});
